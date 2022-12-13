d26ea080fdae3961df976432aeb6ee0fc8c13675
Henrique André <henrique_andre10@hotmail.com>
2022-12-13 11:15:17 -0300

    movement implemented, missing fix bugs, direcional, increasing movement

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <curses.h> // manipulação de tela, 
 #include <cstdlib> // para a função rand();
 #include <utility> // para usar a função pair que armazena a localização de cada parte da cobra
-#include <list> // Vector permite uma rapida movimentação no primeiro e último termo
+#include <list> // list permite uma rapida movimentação no primeiro e último termo
 
 using namespace std;
 
@@ -22,11 +22,12 @@ int main() {
     int dir;
     int premioX, premioY;
     int score=0, cont=0;
-    int snakelen=1;
+    int snakelen=5;
 
     list <pair<int, int>> snakePosi;
-    snakePosi.push_back(make_pair(1,2));
-    list<pair<int, int>>::iterator itSnakePosi = snakePosi.begin();
+    snakePosi.push_back(make_pair(hAltura,hComprimento));
+    list<pair<int, int>>::iterator itSnakePosi = snakePosi.end(), itSnakeAux = snakePosi.end();
+    int aux;
 
     //using keypad is possible get direcional keys
     keypad(stdscr, TRUE);
@@ -36,28 +37,58 @@ int main() {
         // movimentação
         // 4 - LEFT ; 5 - RIGHT ; 2 - DOWN ; 3 - UP
         if ((mv == 4 || mv == 5 || mv == 2 || mv == 3) && !isPressed){
-            isPressed=true;
-            
+            isPressed=true;  
         }
         if (mv == 4) {dir = 4;} // LEFT
         else if (mv == 5) {dir = 3;} // RIGHT
         else if (mv == 2) {dir = 1;} // DOWN
         else if (mv == 3) {dir = 2;} // UP
 
+        // itSnakePosi--;
+        // itSnakeAux--;
+
         // move e apaga o anterior 
-        for (int i=0; i<snakelen; i++){
-        if (isPressed && dir == 1) { hAltura ++; mvprintw(hAltura-1, hComprimento, " "); mvprintw(hAltura, hComprimento, "%c", 219); }
-        else if (isPressed && dir == 2) { hAltura --; mvprintw(hAltura+1, hComprimento, " "); mvprintw(hAltura, hComprimento, "%c", 219);}
-        else if (isPressed && dir == 3) { hComprimento ++; mvprintw(hAltura, hComprimento-1, " "); mvprintw(hAltura, hComprimento, "%c", 219);}
-        else if (isPressed && dir == 4) { hComprimento --; mvprintw(hAltura, hComprimento+1, " "); mvprintw(hAltura, hComprimento, "%c",219);}
+        clear();
+        if (isPressed && dir == 1) { 
+            hAltura++;
+            while(itSnakePosi!=snakePosi.end()){
+            mvprintw(itSnakePosi->first, itSnakePosi->second, "%c", 219); 
+            itSnakePosi++;
+            }
         }
+        else if (isPressed && dir == 2) { 
+            hAltura --;  
+            while(itSnakePosi!=snakePosi.end()){
+            mvprintw(itSnakePosi->first, itSnakePosi->second, "%c", 219); 
+            itSnakePosi++;
+            }
+        }
+        else if (isPressed && dir == 3) { 
+            hComprimento ++;  
+            while(itSnakePosi!=snakePosi.end()){
+            mvprintw(itSnakePosi->first, itSnakePosi->second, "%c", 219); 
+            itSnakePosi++;
+            }
+        }
+        else if (isPressed && dir == 4) { 
+            hComprimento --;  
+            while(itSnakePosi!=snakePosi.end()){
+            mvprintw(itSnakePosi->first, itSnakePosi->second, "%c", 219); 
+            itSnakePosi++;
+            }
+        }
+        
+        //itSnakePosi = snakePosi.begin();
+        itSnakePosi->first=hAltura; itSnakePosi->second=hComprimento;
+
 
         //define o local onde o jogador tem qeu ir para ganhar pontos
         if (score == 0 && cont == 0){ premioX = rand()%altura; premioY = rand()%comprimento; cont ++;}
-        else if (hAltura==premioX && hComprimento==premioY) { score ++; premioX = rand()%altura; premioY = rand()%comprimento; }
+        else if (hAltura==premioX && hComprimento==premioY) { score ++; premioX = rand()%altura; premioY = rand()%comprimento; snakePosi.push_back(make_pair(altura,comprimento)); }
         mvprintw(premioX, premioY, "p");
-        napms(100);
+        napms(150);
         mvprintw(0, 0, "%d", score); // mostra o score
+        
 
 
         snakePosi.push_front(make_pair(hAltura, hComprimento));
